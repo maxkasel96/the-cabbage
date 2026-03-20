0fa29dfcaf750a3922c7c96bab7b90655b311cea
maxkasel96 <56853252+maxkasel96@users.noreply.github.com>
2026-03-20 09:06:40 -0500

    Unify utility button sizing with FAB

--- app/components/SpotifyPlaylistModal.tsx
+++ app/components/SpotifyPlaylistModal.tsx
@@ -199,7 +199,7 @@ export default function SpotifyPlaylistModal({ className, tabIndex }: SpotifyPla
         <img
           src={SPOTIFY_BUTTON_IMAGE_URL}
           alt=""
-          className="spotify-playlist-button__image"
+          className="floating-utility-button__image"
           aria-hidden="true"
         />
       </button>
